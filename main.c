fc4ef151b2d4bcf359775742b209d3fddd137fe8
Muhtadee Taron <sheikh.taron@northsouth.edu>
2023-06-23 22:35:20 +0600

    included a splash screen prototype

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "menu.h"
+#include "splash.h"
+#include "login.h"
+#include "menuForCustomer.h"
 
 int main(void)
 {
-    menu();
+    splash();
+    login();
+
 
     return 0;
 }
---
+++ splash.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#define SIZE 128
+
+void print_image(FILE *fptr);
+
+int splash(void)
+{
+    char *filename = "welcome.txt";
+    FILE *fptr;
+
+    fptr = fopen(filename, "r");
+
+    if(fptr == NULL)
+    {
+        fprintf("error opening %s\n", filename);
+        return 1;
+    }
+
+    print_image(fptr);
+
+    fclose(fptr);
+
+    sleep(2);
+    system("cls");
+
+    return 0;
+}
+
+void print_image(FILE *fptr)
+{
+    char read_string[SIZE];
+
+    while(fgets(read_string, sizeof(read_string), fptr) != NULL)
+        printf("\t\t\t\t\t\t%s", read_string);
+}
+
